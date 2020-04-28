7d24ec8a86c5cc53e073a189313415f3ab9e1ed5
Mateusz Knaga <gwynbleidd89542@gmail.com>
2020-04-28 10:16:03 +0200

    first commit

---
+++ Zad2/Assets/Scripts/Collision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+         void OnCollisionEnter2D(Collision2D other)
+     {
+        if (other.gameObject.name == "Girl")
+            SceneManager.LoadScene("EndGame");
+     }
+ }
+
---
+++ Zad2/Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+   
+    public GameObject objectToSpawn;
+
+    void Start()
+    {
+      
+        InvokeRepeating("SpawnObject", 0f, 4f);
+    }
+   
+    void SpawnObject()
+    {
+        Instantiate(objectToSpawn, new Vector3(-10.0f, 30f, 0), Quaternion.identity);
+    }
+}
